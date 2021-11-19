3fa2e1c741b62b697d58331312186f659be8d1a4
sujin-create <qsoojinq@naver.com>
2021-11-20 03:18:15 +0900

    feature : qr_generattor

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # qrcode
+
+## qrcode generator를 만드는 방법으로 QR Codes Generator API를 활용합니다.
+##### =>  data값에 원하는 값을 넣으면 되고 이후 계획한 웹사이트에서 사용자데이터를 보여줄 것이고 그 주소를 넣을 예정입니다.
+
+
+
---
+++ main.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector('.btn');
+var code = document.querySelector('.code');
+var toast = document.querySelector('#toast');
+btn.addEventListener("click",generate);
+function generate() {
+   
+    var data2 = 22//33을 google에 검색하는 것과 동일함. ->url이 생성됨. => 사용자의 정보가 담긴 (이름, 주소 등) html정보를 보여줄 때 
+    var data3 =44
+    //URL을 우리 홈페이지로 이어주고 사용자데이터를 홈페이지에서 제공하게 끔하면 됨. =>  var data3 = 홈페이지주소/DB에 저장된 사용자 ID값(pK값 아무거나)
+    var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data3}`;
+    code.src = url;
+    toastDiv();
+}
+function toastDiv() {
+    toast.className = "show";
+    setTimeout( function() {
+        toast.className = toast.className.replace("show" , "");
+    },2000);
+}
